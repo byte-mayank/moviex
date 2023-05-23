33abc81dbd8a9e6a4b74382ce4e0cca76ebf679b
mat943 <madhukarmayank93@gmail.com>
2023-05-24 00:56:50 +0530

    confirmation of getting pages from url

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import { useState, useEffect } from "react";
+import fetchDataFromApi from "./utils/api";
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice";
+
 function App() {
-  return <div className="App">App</div>;
+  const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
+  console.log(url);
+
+  useEffect(() => {
+    apiTesting();
+  }, []);
+
+  const apiTesting = () => {
+    fetchDataFromApi("/movie/popular").then((res) => {
+      console.log(res);
+      dispatch(getApiConfiguration(res));
+    });
+  };
+
+  return (
+    <div className="App">
+      App
+      {url?.total_pages}
+    </div>
+  );
 }
 
 export default App;
