32c591cf751686dc606af181fde2636e952e1a9f
mat943 <madhukarmayank93@gmail.com>
2023-05-24 14:31:59 +0530

    create route using react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import fetchDataFromApi from "./utils/api";
 import { useSelector, useDispatch } from "react-redux";
 import { getApiConfiguration } from "./store/homeSlice";
+
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import pageNotFound from "./pages/404/pageNotFound";
 
 function App() {
   const dispatch = useDispatch();
@@ -20,10 +29,11 @@ function App() {
   };
 
   return (
-    <div className="App">
-      App
-      {url?.total_pages}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
