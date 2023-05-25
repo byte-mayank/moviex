278bf62a74237f1142b4a5bea68561a15aea384c
mat943 <madhukarmayank93@gmail.com>
2023-05-25 14:47:57 +0530

    Enable Header and Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -18,18 +18,26 @@ function App() {
   console.log(url);
 
   useEffect(() => {
-    apiTesting();
+    fetchApiConfig();
   }, []);
 
-  const apiTesting = () => {
-    fetchDataFromApi("/movie/popular").then((res) => {
+  const fetchApiConfig = () => {
+    fetchDataFromApi("/configuration").then((res) => {
       console.log(res);
-      dispatch(getApiConfiguration(res));
+
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      };
+
+      dispatch(getApiConfiguration(url));
     });
   };
 
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:mediaType/:id" element={<Details />} />
@@ -37,6 +45,7 @@ function App() {
         <Route path="/explore/:mediaType" element={<Explore />} />
         <Route path="*" element={<pageNotFound />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
