f514ac07219003e868a3a6784e69230022a7ca0b
mat943 <madhukarmayank93@gmail.com>
2023-05-26 17:40:15 +0530

    change endpoint spelling

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -7,18 +7,18 @@ import useFetch from "../../../hooks/useFetch";
 import Carousel from "../../../components/carousel/Carousel";
 
 const Trending = () => {
-  const [endPoint, setEndPoint] = useState("day");
+  const [endpoint, setEndpoint] = useState("day");
 
-  const { data, loading } = useFetch(`/trending/all/${endPoint}`);
+  const { data, loading } = useFetch(`/trending/all/${endpoint}`);
 
   const onTabChange = (tab) => {
-    setEndPoint(tab === "Day" ? "day" : "week");
+    setEndpoint(tab === "Day" ? "day" : "week");
   };
 
   return (
     <div className="carouselSection">
       <ContentWrapper>
-        <span className="carouselTtile">Trending</span>
+        <span className="carouselTitle">Trending</span>
         <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
       </ContentWrapper>
       <Carousel data={data?.results} loading={loading} />
